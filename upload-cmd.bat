59fa8bc454c317aaa378f79aac14edc1c4665b11
zhengxuqiao <1965913741@qq.com>
2022-08-12 08:07:10 +0800

    test

--- upload-cmd.bat
+++ upload-cmd.bat
@@ -1,5 +1,6 @@
 
 pushd E:\族谱\族谱本地文件夹\家谱文件\跳板网页文件\自建网页\WWW\zhengxuqiao.github.io
+
 ::echo 当前盘符路径：%~dp0
 ::echo 当前路径下文件：
 ::dir /b
@@ -9,4 +10,3 @@ git commit -m "test"
 git push -u origin master -f
 
 popd
-
