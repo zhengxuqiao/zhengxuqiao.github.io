4b205121a962f14f223a3bd8aa330c80f1e44338
zhengxuqiao <1965913741@qq.com>
2023-05-31 23:16:22 +0800

    test

--- js/viewer.js
+++ js/viewer.js
@@ -273,7 +273,7 @@
      * Define the max ratio of the image when zoom in.
      * @type {number}
      */
-    maxZoomRatio: 100,
+    maxZoomRatio: 1000,
     /**
      * Define where to get the original image URL for viewing.
      * @type {string | Function}
